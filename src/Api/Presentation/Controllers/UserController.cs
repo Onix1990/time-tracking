7da18d7b4bd0fd220578cc583ca4b19cdfb11c26
JagerJromov <isg.abakan@gmail.com>
2020-09-19 21:04:22 +0700

    user create, update isolation level up

--- src/Api/Presentation/Controllers/UserController.cs
+++ src/Api/Presentation/Controllers/UserController.cs
@@ -1,6 +1,10 @@
-﻿using Api.Domain.Dto;
+﻿using System.Data;
+using System.Threading.Tasks;
+using Api.Common.ActionFilters;
+using Api.Domain.Dto;
 using Api.Domain.Entities;
 using Api.Domain.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Presentation.Controllers {
     public class UserController :
@@ -10,5 +14,19 @@ namespace Api.Presentation.Controllers {
             CreateUserDto, UpdateUserDto, UserOutDto
         > {
         public UserController(IUserService service) : base(service) { }
+
+        [UowType(IsolationLevel.Serializable)]
+        public override Task<ActionResult<UserOutDto>> CreateAsync(
+            CreateUserDto inDto
+        ) {
+            return base.CreateAsync(inDto);
+        }
+
+        [UowType(IsolationLevel.Serializable)]
+        public override Task<ActionResult<UserOutDto>> UpdateAsync(
+            long id, UpdateUserDto inDto
+        ) {
+            return base.UpdateAsync(id, inDto);
+        }
     }
 }
